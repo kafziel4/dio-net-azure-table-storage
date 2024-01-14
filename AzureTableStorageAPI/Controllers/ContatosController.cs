2afd5e30672b1445e46407613d6c3574e5027015
Keoma Kevin Baudin <keoma4@gmail.com>
2024-01-13 23:42:11 -0300

    Add project files.

---
+++ AzureTableStorageAPI/Controllers/ContatosController.cs
@@ -0,0 +1,79 @@
+﻿using Azure.Data.Tables;
+using AzureTableStorageAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureTableStorageAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContatosController : ControllerBase
+    {
+        private readonly string _connectionString;
+        private readonly string _tableName;
+
+        public ContatosController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetValue<string>("SAConnectionString")!;
+            _tableName = configuration.GetValue<string>("AzureTableName")!;
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            var tableClient = GetTableClient();
+
+            contato.RowKey = Guid.NewGuid().ToString();
+            contato.PartitionKey = contato.RowKey;
+
+            tableClient.UpsertEntity(contato);
+
+            return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(string id, Contato contato)
+        {
+            var tableClient = GetTableClient();
+            var contatoTable = tableClient.GetEntity<Contato>(id, id).Value;
+
+            contatoTable.Nome = contato.Nome;
+            contatoTable.Telefone = contato.Telefone;
+            contatoTable.Email = contato.Email;
+
+            tableClient.UpsertEntity(contatoTable);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult ObterTodos([FromQuery] string? nome)
+        {
+            var tableClient = GetTableClient();
+            List<Contato> contatos;
+
+            if (string.IsNullOrWhiteSpace(nome))
+                contatos = [.. tableClient.Query<Contato>()];
+            else
+                contatos = [.. tableClient.Query<Contato>(c => c.Nome == nome)];
+
+            return Ok(contatos);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(string id)
+        {
+            var tableClient = GetTableClient();
+            tableClient.DeleteEntity(id, id);
+            return NoContent();
+        }
+
+        private TableClient GetTableClient()
+        {
+            var serviceClient = new TableServiceClient(_connectionString);
+            var tableClient = serviceClient.GetTableClient(_tableName);
+
+            tableClient.CreateIfNotExists();
+            return tableClient;
+        }
+    }
+}
---
+++ AzureTableStorageAPI/Models/Contato.cs
@@ -0,0 +1,17 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace AzureTableStorageAPI.Models
+{
+    public class Contato : ITableEntity
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Telefone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public string PartitionKey { get; set; } = string.Empty;
+        public string RowKey { get; set; } = string.Empty;
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
